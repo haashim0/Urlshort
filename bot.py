86823d6cf7f12e0144cf71eac425be5df79c286a
kevinnadar <jesikamaraj@gmail.com>
2022-08-01 18:32:51 +0530

    Initial Comiit

---
+++ bot.py
@@ -0,0 +1,85 @@
+import datetime
+import asyncio
+from pyrogram import Client
+from config import *
+from database import db
+from helpers import temp, ping_server
+from utils import broadcast_admins
+
+import logging
+import logging.config
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+
+
+if REPLIT:
+    from flask import Flask, jsonify
+    from threading import Thread
+    
+    app = Flask('')
+    
+    @app.route('/')
+    def main():
+        
+        runtime = datetime.datetime.now()
+        t = runtime - temp.START_TIME
+        runtime = str(datetime.timedelta(seconds=t.seconds))
+        
+        res = {
+            "status":"running",
+            "hosted":"replit.com",
+            "repl":REPLIT,
+            "bot":temp.BOT_USERNAME,
+            "runtime":runtime
+        }
+        
+        return jsonify(res)
+
+    def run():
+      app.run(host="0.0.0.0", port=8000)
+    
+    async def keep_alive():
+      server = Thread(target=run)
+      server.start()
+
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+        "shortener",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins=dict(root="plugins")
+        )
+
+    async def start(self): 
+
+
+        if REPLIT:
+            await keep_alive()
+            
+            asyncio.create_task(ping_server())
+ 
+        temp.START_TIME = datetime.datetime.now()
+        await super().start()
+        me = await self.get_me()
+        self.username = '@' + me.username
+        temp.BOT_USERNAME = me.username
+        temp.FIRST_NAME = me.first_name
+
+        if not await db.get_bot_stats():
+            await db.create_stats()
+        await db.get_announcements()  
+        await broadcast_admins(self, f'** {self.username} Bot started successfully **')
+        logging.info(f'{self.username} Bot started')
+
+
+    async def stop(self, *args):
+        await broadcast_admins(self, '** Bot Stopped Bye **')
+        await super().stop()
+        logging.info('Bot Stopped Bye')
+
---
+++ translation.py
@@ -0,0 +1,147 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_MESSAGE = '''Hello, {}
+
+I'm a Bot to Short Other Links to du-link.in Links by Using your API. 
+
+Just Send me Any Post with Other Links. I will Convert Those Links Using Your API and Send them Back To You
+
+Hit /help for more information about this Bot
+'''
+
+
+HELP_MESSAGE = '''
+Hey! My name is {firstname}. I am a Link Shortener Bot, here to make your Work Easy and Help you to Earn more
+
+I have lots of handy features, such as 
+
+- [Hyperlink](https://t.me/{username})
+- Buttons convert support
+- Header and Footer Text support
+- Replace Username
+- Banner Image
+
+Helpful commands:
+
+- /start: Starts me! You've probably already used this.
+- /help: Sends this message; I'll tell you more about myself!
+
+If you have any bugs or questions on how to use me, contact to {owner}.
+
+Below are some features I provide'''
+
+
+ABOUT_TEXT = """
+**My Details:**
+
+`🤖 Name:` ** {} **
+    
+`📝 Language:` [Python 3](https://www.python.org/)
+`🧰 Framework:` [Pyrogram](https://github.com/pyrogram/pyrogram)
+`👨‍💻 Developer:` [Dev](t.me/ask_admin001)
+`📢 Support:` [Talk Bot](https://t.me/t2linkspromotion_bot)
+`🌐 Source Code:` [GitHub](https://github.com/T2links)
+"""
+
+
+
+HELP_REPLY_MARKUP = InlineKeyboardMarkup([
+
+    [
+        InlineKeyboardButton('Home', callback_data='start_command')
+    ]
+])
+
+
+ABOUT_REPLY_MARKUP = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton('Home', callback_data=f'start_command'),
+        InlineKeyboardButton('Help', callback_data=f'help_command')
+    ],
+    [
+        InlineKeyboardButton('Close', callback_data='delete')
+    ]
+])
+
+START_MESSAGE_REPLY_MARKUP  = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton('Announcement', callback_data='anc_command'),
+        InlineKeyboardButton('Help', callback_data=f'help_command'),
+        
+    ],
+        [
+        InlineKeyboardButton('About', callback_data='about_command'),
+        InlineKeyboardButton('Close', callback_data='delete')
+    ],
+
+])
+
+
+BACK_REPLY_MARKUP = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton('Back', callback_data=f'start_command')
+    ],
+
+])
+
+USER_ABOUT_MESSAGE = """
+- Shortener Website: {base_site}
+
+- {base_site} API: {shortener_api}
+
+- Username: @{username}
+
+- Header Text: 
+{header_text}
+
+- Footer Text: 
+{footer_text}
+
+- Banner Image: {banner_image}
+"""
+
+
+SHORTENER_API_MESSAGE = """To add or update your Shortner Website API, `/shortener_api api`
+            
+Ex: `/shortener_api 6LZq851sXofffPHugiKQq`
+            
+Shortener API of your preferred shortener API.
+
+Current Website: {base_site}
+
+Current Shortener API: `{shortener_api}`"""
+
+HEADER_MESSAGE = """Reply to the Header Text You Want
+
+This Text will be added to the top of every message caption or text
+
+To Remove Header Text: `/header remove`"""
+
+FOOTER_MESSAGE = """Reply to the Footer Text You Want
+
+This Text will be added to the bottom of every message caption or text
+
+To Remove Footer Text: `/footer remove`"""
+
+USERNAME_TEXT = """Current Username: {username}
+
+Usage: `/username your_username`
+
+For just removing the username from the post: 
+`/username none`
+
+This username will be automatically replaced with other usernames in the post
+
+To remove current username, `/username remove`"""
+
+BANNER_IMAGE = """Current Banner Image URL: {banner_image}
+
+Usage: `/banner_image image_url`
+
+This image will be automatically replaced with other images in the post
+
+To remove custom image, `/banner_image remove`
+
+Eg: `/banner_image https://www.nicepng.com/png/detail/436-4369539_movie-logo-film.png`"""
+
