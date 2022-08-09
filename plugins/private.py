6578e17b5ca1eabd721aecab8cacad7892ac8484
Gauravbots <102266943+Gauravbots@users.noreply.github.com>
2022-08-09 22:34:18 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -52,9 +52,5 @@ async def private_link_handler(c:Client, message:Message):
         if BIN_CHANNEL: 
             await c.send_message(BIN_CHANNEL, text=bin_caption)
 
-    except Exception as e:
-        await message.reply("😳 Error while trying to convert links %s:" % e, quote=false)
-        logger.exception(e)
-    finally:
-        await txt.delete()
+    
 
