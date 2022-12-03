a133ab507a6075376e970baffe8874bca6a494d8
Gauravbots <102266943+Gauravbots@users.noreply.github.com>
2022-12-03 05:38:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,17 +9,17 @@ def is_enabled(value, default):
         return default
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID", "13956112"))
+API_HASH = os.environ.get("API_HASH", "a14d6d445c6be0a22f4a5ad401e0eab6")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5274278212:AAHWEcoMgFpNVyGVuB4wZfmVkud1rT5Tdgo")
 ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
-OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "shortener")
-DATABASE_URL = os.environ.get("DATABASE_URL", None)
+OWNER_ID = int(os.environ.get("OWNER_ID", "2062513342"))
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "DulinkShortBot")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://DulinkShortBot:DulinkShortBot@cluster0.f5z3pon.mongodb.net/?retryWrites=true&w=majority")
 # Optionnal variables
-BROADCAST_AS_COPY = is_enabled((os.environ.get('BROADCAST_AS_COPY', "False")), False)
+BROADCAST_AS_COPY = is_enabled((os.environ.get('BROADCAST_AS_COPY', "True")), False)
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '')
-BIN_CHANNEL = os.environ.get("BIN_CHANNEL", None)
+BIN_CHANNEL = os.environ.get("BIN_CHANNEL", "-1001724014855")
 #  Replit Config
 REPLIT_USERNAME = os.environ.get("REPLIT_USERNAME", None)
 REPLIT_APP_NAME = os.environ.get("REPLIT_APP_NAME", None)
